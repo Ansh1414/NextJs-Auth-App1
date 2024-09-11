31e1dbb21ad25a06154a2befcf72ddbd8de2838b
dev ak <ak@gmail.com>
2024-09-12 01:49:53 +0530

    images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,54 @@
 "use Client"
 import Link from "next/link";
-
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Ankush Nextjs</h1>
-        <Link href="/signup">Visit SignUp page</Link>
-        
-        </main>
+   
+    <>
+    <div className="mx-auto max-w-7xl px-2 md:px-0">
+  <div className="my-4">
+    <h1 className="text-3xl font-bold">Our Team</h1>
+    <p className="mt-2 text-gray-500">
+      Hi every one 
+      
+    </p>
+  </div>
+  <div className="grid grid-cols-1 gap-[50px] md:grid-cols-2">
+    <div className="flex flex-col items-center text-start">
+      <div
+        className="relative flex h-[342px] w-full flex-col justify-end rounded-[10px] bg-red-300"
+      >
+        <img
+          src="/images/ankush.png"
+          alt=""
+          className="z-0 h-full w-full rounded-[10px] object-cover"
+        />
+        <div className="absolute bottom-4 left-4">
+          <h1 className="text-xl font-semibold text-white">Ankush Sharma</h1>
+          <h6 className="text-base text-white">Frontend Backend Developer</h6>
+        </div>
+      </div>
     </div>
+    <div className="flex flex-col items-center text-start">
+      <div
+        className="relative flex h-[342px] w-full flex-col justify-end rounded-[10px] bg-red-300"
+        
+      >
+        <img
+          src="/images/ankita.png"
+          alt=""
+          className="z-0 h-full w-full rounded-[10px] object-cover"
+        />
+        <div className="absolute bottom-4 left-4">
+          <h1 className="text-xl font-semibold text-white">Ankita Sharma</h1>
+          <h6 className="text-base text-white">Marketing head</h6>
+        </div>
+      </div>
+    </div>
+    
+    <Link href="/signup">Visit SignUp page</Link>
+  </div>
+</div>
+    </>
   );
 }
